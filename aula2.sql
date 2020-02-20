76cbd8ccc98484c3fedd8db74dd7988966a6223b
Dilenon Delfino <stefan.dilenon@gmail.com>
2020-02-19 21:28:45 -0300

    sgdr, para virar nosql

--- aula2.sql
+++ aula2.sql
@@ -1,4 +1,11 @@
 --Oracle Database
+drop table conta;
+drop table agencia;
+drop table cidade;
+drop table estado;
+drop table banco;
+drop table cliente;
+drop table lancamento;
 
 create table banco(
     num_banco number(4) primary key,
@@ -48,18 +55,21 @@ create table conta(
     status char(1) not null,
     cpf number(11) not null,
     constraint ck_status_conta check (status in ('s', 'n')),
-    constraint pk_cc primary key (num_conta, num_conta, num_ag),
+    constraint pk_cc primary key (num_conta, num_banco, num_ag),
     constraint fk_cc_ag foreign key (num_banco, num_ag)
-        references agencia(num_banco, num_ag),
+        references agencia(num_banco, n
     constraint fk_cc_cli foreign key (cpf)
         references cliente(cpf)
 );
 
 create table lancamento(
     num_conta number(8),
+    num_ag number(5),
+    num_banco number(4),
     datahora date not null,
     descricao varchar2(120),
-    constraint pk_dt_lan primary key (datahora, num_conta),
-    constraint fk_lan_cc foreing key (num_conta)
-        references conta(num_conta)
+    valor number(10,2) not null,
+    constraint pk_dt_lan primary key (datahora, num_conta, num_ag),
+    constraint fk_lan_cc foreign key (num_conta, num_ag, num_banco)
+        references conta(num_conta, num_ag, num_banco)
 );
